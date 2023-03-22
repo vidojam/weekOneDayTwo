4cb99a587a0acd04b3c0854da5179034ab7f8612
Jose Torres <vidojam@gmail.com>
2023-03-21 22:38:45 -0400

    Homework weekOneDayTwo.js - Switch,ElseIf,Sum

---
+++ variables.js
@@ -0,0 +1,19 @@
+// assignment #1 & 2
+const country = "United States";
+const continent = "North America";
+
+console.log(country)
+console.log (continent)
+
+// assigment #3
+const pet = "miel";
+
+// assignment #4
+let x = "15";
+let y = 15;
+
+if (x === y) {
+    console.log("x & y are the same value & data type");
+} else {
+    console.log("x & y are not the same data type");
+}
---
+++ weekOneDayTwo.js
@@ -0,0 +1,45 @@
+// animal and sounds
+function animalSound() {
+let animal = "cat"
+switch (animal) {
+    case "wolf":
+        console.log ("woof");
+        break
+
+    case "cat":
+        console.log ("meow");
+        break
+
+    case "dog":
+        console.log ("bow wow");
+        break
+    }
+}
+animalSound();
+
+// fruit color
+function fruitColor() {
+    let fruit = "orange";
+    if (fruit === "lemon") {
+    console.log("The color of a lemon is yellow");
+    } else if (fruit === "banana") {
+    console.log("The color of a banana is yellow");
+    } else if (fruit === "avocado") {
+    console.log("The color of a avocado is green");
+    } else {   
+    console.log("The color of a orange is orange");
+    }
+}    
+fruitColor();
+
+
+// The sum of two numbers
+
+function sum() {
+    let num1 = 10;
+    let num2 = 15;
+    console.log(num1 + num2);
+}
+
+sum();
+
